2500ec15d1ee5f32f36f3383f655031946c38a52
fawkes <fawkes@faw-kes.de>
2018-07-30 22:20:39 +0200

    removed optimizer -> use https://github.com/PoC-Consortium/engraver removed plotter -> use https://github.com/PoC-Consortium/engraver removed orphaned stuff from helper.* fixed Makefile to run on debian 9 - test on other distros welcome added gitignore to prevent binary commits

--- helper.c
+++ helper.c
@@ -45,32 +45,6 @@ int xstr2strr(char *buf, unsigned bufsize, const char *in) {
 }
 // END (taken from)
 
-// Detect number of CPUs (by Dirk-Jan Kroon)
- 
-int getNumberOfCores() {
-#ifdef WIN32
-    SYSTEM_INFO sysinfo;
-    GetSystemInfo(&sysinfo);
-    return sysinfo.dwNumberOfProcessors;
-#elif MACOS
-    int nm[2];
-    size_t len = 4;
-    uint32_t count;
- 
-    nm[0] = CTL_HW; nm[1] = HW_AVAILCPU;
-    sysctl(nm, 2, &count, &len, NULL, 0);
- 
-    if(count < 1) {
-    nm[1] = HW_NCPU;
-    sysctl(nm, 2, &count, &len, NULL, 0);
-    if(count < 1) { count = 1; }
-    }
-    return count;
-#else
-    return sysconf(_SC_NPROCESSORS_ONLN);
-#endif
-}
-
 // From http://www.binarytides.com/hostname-to-ip-address-c-sockets-linux/
 int hostname_to_ip(char * hostname , char* ip) {
     struct hostent *he;
@@ -91,29 +65,5 @@ int hostname_to_ip(char * hostname , char* ip) {
     }
     
     return 1;
-}
-
-unsigned long long freespace(char *path) {
-	struct statvfs fData;
-
-	if(statvfs(path,&fData) != 0) {
-		printf("Failed to get free disk space on %s\n", path);
-		return 0;
-	}
-	return (unsigned long long)fData.f_bsize * fData.f_bfree;
-}
-
-// Free memory. Taken from http://stackoverflow.com/questions/2513505/how-to-get-available-memory-c-g
-unsigned long long freemem() {
-#ifdef _WIN32
-	MEMORYSTATUSEX status;
-	status.dwLength = sizeof(status);
-	GlobalMemoryStatusEx(&status);
-	return status.ullTotalPhys;
-#else
-	long pages = sysconf(_SC_PHYS_PAGES);
-	long page_size = sysconf(_SC_PAGE_SIZE);
-	return (unsigned long long)pages * page_size;
-#endif
 }
 
